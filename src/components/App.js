937171f142402a24b4671a6898b0d89272dcf8f7
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2023-01-16 08:56:22 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+
+import React,{useState, useEffect} from "react";
+
+
+function App() {
+    const [dogSan, setDogSan]= useState(null)
+    
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response)=>response.json())
+        .then((data)=>{
+            setDogSan(data.message);
+        })
+
+    }, []);
+
+    if(!dogSan)return<p>Loading...</p>
+
+    return<img  src={dogSan} alt="A Random Dog"/>
+}
+export default App;
